5854897804ab2a981a0dbaa8697767ac8eae0244
Joe Begley <joebegley15@gmail.com>
2018-06-12 10:21:10 -0700

    Added the solution to a problem.:

---
+++ replace_char_w_alph_pos/main.js
@@ -0,0 +1,11 @@
+function alphabetPosition(text) {
+  var arr = [];
+  for (var i = 0; i < text.length; i++) {
+    if (text.charCodeAt(i) >= 65 && text.charCodeAt(i) <= 90) {
+      arr.push(text.charCodeAt(i) - 64);
+    } else if (text.charCodeAt(i) >= 97 && text.charCodeAt(i) <= 122) {
+      arr.push(text.charCodeAt(i) - 96);
+    }
+  }
+  return arr.join(' ');
+}
