e3ae31e783292464452de396e606a3410e0e56e0
Joe Begley <joebegley15@gmail.com>
2018-06-26 14:29:32 -0400

    More efficient solution added

--- count_change_combos/main.js
+++ count_change_combos/main.js
@@ -2,13 +2,12 @@ function sortNumber(a,b) {
     return b - a;
 }
 
-function countChange(value, coins) {
+function listChange(value, coins) {
 	var arr = [],total;
 	coins = coins.sort(sortNumber);
 	function combos(value,coins,used) {
 		coins.map(function(coin,i){
 			total = used.reduce((a,b) => a + b, 0);
-			console.log(total + coin,used);
 			if (total + coin == value) {
 				total + coin == value;
 				arr.push(used.concat([coin]))
@@ -18,7 +17,25 @@ function countChange(value, coins) {
 		})
 	}
 	combos(value,coins,[])
-	return arr.length;
+	return arr;
 }
 
-console.log(countChange(10, [5,1,10]));
+function numberOfCombos(value, coins) {
+	var v = 0,total;
+	function combos(value,coins,used) {
+		coins.map(function(coin,i){
+			total = used.reduce((a,b) => a + b, 0);
+			if (total + coin == value) {
+				total + coin == value;
+				v++;
+			} else if (total + coin < value) {
+				combos(value,coins.slice(i),used.concat([coin]));
+			}
+		})
+	}
+	combos(value,coins,[])
+	return v;
+}
+
+console.log(listChange(10, [5,1,10]));
+console.log(numberOfCombos(10, [5,1,10]));
