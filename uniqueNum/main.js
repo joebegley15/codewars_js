2662bb6bbfe8e3822e76e8877ec5aa59e64726c8
Joe Begley <joebegley15@gmail.com>
2018-07-13 11:03:38 -0400

    Solution added

--- uniqueNum/main.js
+++ uniqueNum/main.js
@@ -1,17 +1,22 @@
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
+
 function findUniq(arr) {
+  console.log(arr);
   const hash = {}
   let returnMe;
   for (var i = 0; i < arr.length; i++) {
     hash[arr[i]] = hash[arr[i]] + 1 || 0;
-    if (hash[arr[i]]) {
-      Object.keys(hash).map(function(key){
-        if (!hash[key]) {
-          returnMe = key;
-        }
-      })
-      if (returnMe) {
-        return parseInt(returnMe);
-      }
+    console.log('-',arr[i],hash[arr[i]]);
+    if (hash[arr[i]] == 0 && Object.size(hash) == 2) {
+      return arr[i]
     }
   }
 }
+
+console.log(findUniq([ 0, 1, 1, 1, 1, 1, 1, 1 ]));
