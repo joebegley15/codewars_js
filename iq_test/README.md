b81f25f8142f9668f4605393b64efc8cea499ceb
Joe Begley <joebegley15@gmail.com>
2018-07-16 14:19:01 -0400

    Solution added

---
+++ iq_test/README.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/iq-test/train/javascript
---
+++ iq_test/main.js
@@ -0,0 +1,13 @@
+function iqTest(numbers){
+	let odd = 0, even = 0, v;
+	numbers = numbers.split(' ')
+	for (var i = 0; i < numbers.length; i++) {
+		numbers[i] % 2 ? odd++ : even++;
+	}
+	odd > even ? v = 0 : v = 1;
+	for (var i = 0; i < numbers.length; i++) {
+		if (numbers[i] % 2 == v) {return i+1}
+	}
+}
+
+console.log(iqTest('1 2 3 5 7'))
