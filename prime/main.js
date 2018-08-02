0678963ed59379e41715de43088a9e7d39de0772
Joe Begley <joebegley15@gmail.com>
2018-08-02 18:25:44 -0400

    Cleaner, more efficient code

--- prime/main.js
+++ prime/main.js
@@ -1,10 +1,7 @@
 function isPrime(n) {
-  if (n == 2) {
-    return true;
-  } else if (n % 2 == 0 || n <= 1) {
-    return false;
-  }
-  for (var i = 3; i <= Math.sqrt(n); i++) {
+  if (n < 2 || (n % 2 == 0 && n > 2)) return false;
+  for (var i = 3; i <= Math.sqrt(n); i += 2) {
+      console.log(n,i);
     if (n % i == 0) {
       return false;
     }
