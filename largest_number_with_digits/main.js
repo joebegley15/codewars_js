e2aeafc22e34b030ddeaef314db1e5628435dfd3
Joe Begley <joebegley15@gmail.com>
2018-08-07 14:45:04 -0400

    Cleaner solution added

--- largest_number_with_digits/main.js
+++ largest_number_with_digits/main.js
@@ -1,17 +1,1 @@
-function maxNumber(n){
-  function quickSort(arr){
-    var less = [], more = [], index = arr[0];
-    if (arr.length < 2) {
-      return arr;
-    }
-    for (var i = 1; i < arr.length; i++) {
-      if (arr[i] < index) {
-        less.push(arr[i]);
-      } else {
-        more.push(arr[i]);
-      }
-    }
-    return quickSort(more).concat(index,quickSort(less));
-  };
-  return + quickSort(n.toString().split('')).join('');
-}
+const maxNumber = n => +String(n).split('').sort((a,b)=>b-a).join('');
