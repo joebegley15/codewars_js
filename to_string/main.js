60a36586c55f4e9b1b54da8860989216b86133c5
Joe Begley <joebegley15@gmail.com>
2018-08-14 14:11:58 -0400

    Solution added

---
+++ to_string/main.js
@@ -0,0 +1,4 @@
+Number.prototype.toString = function() {return "" + this;}
+Array.prototype.toString = function() {return "[" + this.join(",") + "]";}
+Boolean.prototype.toString = function() {return "" + this;}
+Object.prototype.toString = function() {return "" + this;}
