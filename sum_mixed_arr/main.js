50a7c0e22c2468728e7e27adc39edb8aa32a424a
Joe Begley <joebegley15@gmail.com>
2018-08-18 18:06:34 -0400

    Included both versions of the solution for clarity

--- sum_mixed_arr/main.js
+++ sum_mixed_arr/main.js
@@ -1,1 +1,11 @@
+// One line
 const sumMix = (arr, sum=0) => arr.map(v => parseInt(v)).reduce((a,b)=>a+b);
+
+// Multi - Line
+function sumMix(arr){
+  let sum = 0;
+  arr.map(function(element){
+    sum += parseInt(element);
+  })
+  return sum;
+} 
