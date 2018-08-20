5a6dc31b84c0932109a9d782b56b75558feac9d0
Joe Begley <joebegley15@gmail.com>
2018-08-20 16:08:46 -0400

    Better solution added

--- is_anagram/main.js
+++ is_anagram/main.js
@@ -1,7 +1,20 @@
 // write the function isAnagram
-var isAnagram = function(test, original) {
-  const sortAlph = text => text.toLowerCase().split('').sort().join('');
+const sortAlph = text => text.toLowerCase().split('').sort().join('');
+const isAnagram = function(test, original) {
   test = sortAlph(test);
   original = sortAlph(original);
   return test == original;
 };
+
+const isAnagramOneLine = (test,original) => test.toLowerCase().split('').sort().join('') == original.toLowerCase().split('').sort().join('');
+const optimal = (test,original) => sortAlph(test) == sortAlph(original);
+
+
+console.log(isAnagram('main','maine'));
+console.log(isAnagram('node','done'));
+
+console.log(isAnagramOneLine('main','maine'));
+console.log(isAnagramOneLine('node','done'));
+
+console.log(optimal('main','maine'));
+console.log(optimal('node','done'));
