501ec63a484a74527fa4b7db286de491393415c2
Joe Begley <joebegley15@gmail.com>
2018-10-29 18:38:16 -0700

    Solution added

---
+++ order_words/README.md
@@ -0,0 +1,1 @@
+https://www.codewars.com/kata/55d7e5aa7b619a86ed000070/train/javascript
---
+++ order_words/main.js
@@ -0,0 +1,17 @@
+const orderWord = s =>{
+  if (!s) return 'Invalid String!';
+  s = [...s]
+  const sort = arr => {
+    if (arr.length < 2) return arr;
+    let less = [], more = [], point = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i].charCodeAt(0) < point.charCodeAt(0)) {
+        less.push(arr[i]);
+      } else {
+        more.push(arr[i]);
+      }
+    }
+    return sort(less).concat(point, sort(more))
+  }
+  return sort(s).join('');
+}
