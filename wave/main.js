570d9c912e462ad82196ff6b9729675b2efb3d5a
Joe Begley <joebegley15@gmail.com>
2019-02-11 23:05:03 -0800

    Complete solution added

--- wave/main.js
+++ wave/main.js
@@ -1,7 +1,9 @@
 function wave(str){
   const solution = [];
   for (var i = 0; i < str.length; i++) {
-    solution.push([str.slice(0, i), str.slice(i, i + 1).toUpperCase(), str.slice(i + 1)].join(''))
+    if (/[A-Z]/i.test(str.slice(i, i + 1))) {
+      solution.push([str.slice(0, i), str.slice(i, i + 1).toUpperCase(), str.slice(i + 1)].join(''));
+    }
   }
   return solution;
 }
