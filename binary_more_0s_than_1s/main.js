029a6f8d3ebef1dc32d42d3cb50566b6be778361
Joe Begley <joebegley15@gmail.com>
2020-03-24 23:58:24 -0700

    Fixed type issue in binary solution

--- binary_more_0s_than_1s/main.js
+++ binary_more_0s_than_1s/main.js
@@ -11,17 +11,19 @@ const moreZerosThanOnes = binary => {
   return zeros > ones;
 };
 
-const binaryConverter = str => {
-  return str.toString(2);
+const binaryConverter = n => {
+  console.log(typeof n);
+  return n.toString(2);
 };
 
 const moreZeros = str => {
   const moreZerosLetters = [];
   const strArr = str.split("");
   const usedChars = {};
-  let binary;
+  let charCode, binary;
   strArr.forEach(char => {
-    binary = binaryConverter(char.charCodeAt(0));
+    charCode = char.charCodeAt(0);
+    binary = binaryConverter(charCode);
     if (moreZerosThanOnes(binary) && !usedChars[char]) {
       moreZerosLetters.push(char);
       usedChars[char] = true;
@@ -29,3 +31,5 @@ const moreZeros = str => {
   });
   return moreZerosLetters;
 };
+
+moreZeros("abc");
--- to_binary/main.js
+++ to_binary/main.js
@@ -1,21 +1,19 @@
-function toBinary(n){
-  var str = '';
+function toBinary(n) {
+  var str = "";
   increment = 2;
   if (n === 0) {
     return 0;
   }
   while (n > 0) {
     if (n % increment) {
-      n = n - (increment / 2);
-      str = '1' + str;
+      n = n - increment / 2;
+      str = "1" + str;
     } else {
-      str = '0' + str;
+      str = "0" + str;
     }
     increment *= 2;
   }
-  return parseInt(str);
+  return str;
 }
 
-for (var n = 7; n < 700; n+= 7) {
-  console.log(toBinary(n));
-}
+console.log(toBinary(4));
