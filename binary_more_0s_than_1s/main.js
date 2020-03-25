d15f45dca06a3f59b882e7f2c93735db8f7b56b3
Joe Begley <joebegley15@gmail.com>
2020-03-24 23:52:21 -0700

    Solution added

--- binary_more_0s_than_1s/main.js
+++ binary_more_0s_than_1s/main.js
@@ -1,1 +1,31 @@
-function moreZeros(s) {}
+const moreZerosThanOnes = binary => {
+  let zeros = 0,
+    ones = 0;
+  binary.split("").forEach(num => {
+    if (num === "0") {
+      zeros++;
+    } else {
+      ones++;
+    }
+  });
+  return zeros > ones;
+};
+
+const binaryConverter = str => {
+  return str.toString(2);
+};
+
+const moreZeros = str => {
+  const moreZerosLetters = [];
+  const strArr = str.split("");
+  const usedChars = {};
+  let binary;
+  strArr.forEach(char => {
+    binary = binaryConverter(char.charCodeAt(0));
+    if (moreZerosThanOnes(binary) && !usedChars[char]) {
+      moreZerosLetters.push(char);
+      usedChars[char] = true;
+    }
+  });
+  return moreZerosLetters;
+};
