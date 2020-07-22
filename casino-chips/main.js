2115c87ad5d3fd1ab3ae26cc36d8ef6a49499f47
Joe Begley <joebegley15@gmail.com>
2020-07-22 02:09:38 -0600

    Tweaked solution based on Paul S

--- casino-chips/main.js
+++ casino-chips/main.js
@@ -1,17 +1,46 @@
-const solve = arr => {
-  let totalMoves = 0;
-  arr.sort(function(a, b) {
-    return a - b;
-  });
-  while (arr[1] && arr[2]) {
-    if (arr[1]) {
-      arr[2] = arr[2] - 1;
-      arr[1] = arr[1] - 1;
-      totalMoves++;
+function solve(arr) {
+  var days = 0;
+  var sort = true;
+
+  while (arr[1]) {
+    // only sort if we need to
+    if (sort) {
+      arr.sort((a, b) => b - a);
+      sort = false;
     }
-    arr.sort(function(a, b) {
-      return a - b;
-    });
+
+    if (!arr[1]) {
+      return days;
+    }
+
+    /*
+      Largest stack has at least two, smalest stack has at least one
+      equal number from each stack
+      days used is smallest stack *2
+    */
+    if (arr[0] > 1 && arr[1] > 0 && arr[2] > 0) {
+      arr[0] -= 2;
+      arr[1]--;
+      arr[2]--;
+      days = days + 2;
+
+      // since arr[0] is reduced by 2, we check it vs. arr[1] to see if we need to resort
+      if (arr[0] < arr[1]) {
+        sort = true;
+      }
+    }
+    // if we cannot do 2,1,1 optimization then do 1,1,0 which leads to 1 day
+    else {
+      days++;
+
+      arr[0]--;
+      arr[1]--;
+
+      // if arr[1] is now smaller than arr[2] then we need to resort
+      if (arr[1] < arr[2]) {
+        sort = true;
+      }
+    }
   }
-  return totalMoves;
-};
+  return days;
+}
