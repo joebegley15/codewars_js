307afd74ea23c93044f2dcf3e58b6929c668ec60
Joe Begley <joebegley15@gmail.com>
2020-10-20 00:28:36 -0400

    Better solution

--- larger_product_or_sum/main.js
+++ larger_product_or_sum/main.js
@@ -1,12 +1,6 @@
 const sumOrProduct = (arr, n) => {
   const sorted = arr.sort((a, b) => a - b);
-  const least = sorted.slice(0, n);
-  const most = sorted.slice(arr.length - n, arr.length);
-  const leastProduct = least.reduce((total, el) => total * el, 1);
-  const mostSum = most.reduce((total, el) => total + el, 0);
-  return leastProduct > mostSum
-    ? "product"
-    : leastProduct === mostSum
-      ? "same"
-      : "sum";
+  const least = sorted.slice(0, n).reduce((total, el) => total * el, 1);
+  const most = sorted.slice(-n).reduce((total, el) => total + el, 0);
+  return least > most ? "product" : least === most ? "same" : "sum";
 };
