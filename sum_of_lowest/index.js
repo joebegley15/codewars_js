097d4c0debfd64da6a7e05f4bb439665f1313e73
joebegley15 <joebegley15@gmail.com>
2024-06-11 13:58:39 -0500

    Solution added

---
+++ sum_of_lowest/index.js
@@ -0,0 +1,26 @@
+function sumTwoSmallestNumbers(numbers) {
+    const lowest = [];
+    numbers.forEach(n => {
+        if (lowest[0] === undefined) {
+            lowest.push(n);
+        } else if (lowest[1] === undefined) {
+            if (n < lowest[0]) {
+                lowest.unshift(n);
+            } else {
+                lowest.push(n);
+            }
+        } else {
+            if (n < lowest[0]) {
+                lowest[1] = lowest[0];
+                lowest[0] = n;
+            } else if (n < lowest[1]) {
+                lowest[1] = n;
+            }
+        }
+        console.log(lowest);
+    });
+    console.log(lowest);
+    return lowest[0] + lowest[1];
+}
+
+sumTwoSmallestNumbers([3, 87, 45, 12, 7])
