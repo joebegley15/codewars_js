e577568b33596946cf4b756724247e26cadcfbb2
joebegley15 <joebegley15@gmail.com>
2024-09-03 12:14:48 -0500

    Removed console logs

--- doors/index.js
+++ doors/index.js
@@ -3,14 +3,10 @@ const doors = n => {
     for (let i = 0; i < n; i++) {
         arr.push(0);
     }
-    console.log(arr);
     for (let i = 1; i <= n; i++) {
         for (let x = i - 1; x < n; x += i) {
             arr[x] = arr[x] ? 0 : 1;
         }
-        console.log(arr);
     }
     return arr.reduce((acc, el) => acc += el, 0);
 }
-
-console.log(doors(100));
